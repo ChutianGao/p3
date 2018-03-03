2b696a88513f4d29b2bb11e8f7fdeee20adebcc7
Charlie Gao <chg774@g.harvard.edu>
2018-03-03 17:09:28 -0500

    Add Empty Controller

---
+++ app/Http/Controllers/BillController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class BillController extends Controller
+{
+    /**
+     * GET /
+     */
+    public function index()
+    {
+        return 'Bill Form Displays Here.';
+    }
+}
